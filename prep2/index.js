7e7128fc5f3b848b87865e08dfd264eb24c9b91b
Dylan Babbs <dylan.babbs@here.com>
2019-10-02 15:42:23 +0200

    progress

---
+++ prep2/index.js
@@ -0,0 +1,34 @@
+const GeoJSONWriter = require('../prep/GeoJSONWriter.js');
+const fs = require('fs');
+const Oboe = require('oboe')
+const writer = new GeoJSONWriter('flow_0_0', 'flow', `${__dirname}/test2.json`);
+
+const path = `${__dirname}/berkeley-test.geojson`;
+const stream = fs.createReadStream(path);
+
+const maxes = Array(24).fill(0);
+const parser = async () => {
+   return new Promise(resolve => {
+      Oboe(stream).node('!.features.*', row => {
+         const modified = row;
+         modified.properties.speeds = modified.properties.speeds.map((x,i) => {
+            const value = x[0];
+
+            if (value > maxes[i]) {
+               maxes[i] = value;
+            }
+            return value;
+         });
+         writer.push(modified);
+         return Oboe.drop;
+      }).node('!',resolve);
+   })
+}
+
+(async () => {
+   await parser();
+   console.log(JSON.stringify(maxes))
+   writer.finish();
+})();
+
+
